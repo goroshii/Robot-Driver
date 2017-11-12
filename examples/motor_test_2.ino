0e886d1628d618970956856b2b7757bf0a3b4770
goroshii <32380955+goroshii@users.noreply.github.com>
2017-11-12 13:23:29 -0300

    Create motor_test_2.ino

---
+++ examples/motor_test_2.ino
@@ -0,0 +1,32 @@
+#include <RobotDriver.h> 
+/*for Arduino UNO
+IN1 pin 2
+IN2 pin 4
+IN3 pin7
+IN4 pin6
+EN1 pin 3
+EN2 pin 5*/
+/*Se define la clase RobotDriver*/ 
+
+RobotDriver robot;
+void setup() {
+  /*Se inicializa el objeto robot*/
+  robot.init();
+}
+void loop() {
+  /*robot.avanzar(speed1,speed2), las variables "speedx" están en el rango 0-100*/
+  robot.avanzar(50,30);
+  
+  /*robot.doblar_izq(delay,speed): La variable delay está en segundos, y
+  la variable speed está en un rango 0-100*/
+  robot.doblar_izq(1,70);
+  
+  /*avanzar(speed1,speed2,delay): speed1 y speed2 estan en un rango 0-100, y 
+  la variable delay esta en segundos*/
+  robot.avanzar(40,70,1);
+  
+  robot.doblar_izq(1);
+  
+  robot.avanzar(1);
+
+}
